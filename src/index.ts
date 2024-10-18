29f4ff238ff8b83e29314de60c0c032fa7d20d5b
JAlcon00 <c.jesusalmanza@gmail.com>
2024-10-18 12:03:44 -0600

    se regreso a la funcion basica original

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
 import express from 'express';
 import todoRoutes from './routes/todoRoutes';
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
-// Cargar variables de entorno desde el archivo .env
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuración de Sequelize usando variables de entorno
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql', // o 'postgres', 'sqlite', etc.
-});
-
 app.use(express.json()); // Middleware para procesar JSON
 app.use('/api', todoRoutes); // Verifica que las rutas estén prefijadas correctamente
 
-// Conectar a la base de datos y luego iniciar el servidor
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conexión a la base de datos exitosa.');
-    app.listen(PORT, () => {
-      console.log(`Servidor ejecutándose en el puerto ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error al conectar a la base de datos:', error);
-  });
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
